dddc77843edff75433f0ae4f406fa65da9f23e4a
abhiayasya <168184952+abhiayasya@users.noreply.github.com>
2024-12-06 15:42:02 +0530

    our-clients section initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { About } from "@/components/About";
+import { OurClients } from "@/components/OurClients";
 
 export default function Home() {
   return (
     <div className="bg-black text-white">
       <About/>
+      <h2>our client</h2>
+      <OurClients/>
     </div>
   );
 }
