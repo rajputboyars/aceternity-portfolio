8f6d5ace5c9ebfb01a464090a2febebc29cd3902
abhiayasya <168184952+abhiayasya@users.noreply.github.com>
2024-12-06 12:49:38 +0530

    feature section basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { About } from "@/components/About";
+import { FeatureSection } from "@/components/FeatureSection";
 
 export default function Home() {
   return (
     <div className="bg-black text-white">
-      <About/>
+      <About />
+      <h2 className="text-center">Feature Section</h2>
+      <FeatureSection />
     </div>
   );
 }
