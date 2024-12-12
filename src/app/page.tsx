ed82e8728250e905bc2ddf0b5cf8def16460c47e
abhiayasya <168184952+abhiayasya@users.noreply.github.com>
2024-12-12 12:39:01 +0530

    adding faq component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { About } from "@/components/About";
 import { InfiniteMovingCardsDemo } from "@/components/InfiniteMovingCardsDemo";
 import CardGrid from "@/components/ui/CardGrid";
+import FAQ from "@/components/ui/Faq";
 
 export default function Home() {
   return (
     <div className="bg-black text-white">
       <About/>
       <InfiniteMovingCardsDemo/>
-      
+      <FAQ/>
       <CardGrid />
     </div>
   );
